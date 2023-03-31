f87a7755539abc8b4013ff43519470276184caf7
Kanan Garazada <kanangarayev02@gmail.com>
2023-03-31 17:03:06 +0400

    final endpoint

--- AsynApi/Program.cs
+++ AsynApi/Program.cs
@@ -1,4 +1,6 @@
+using AsyncApi.Dtos;
 using AsyncApi.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +23,38 @@ app.MapPost("api/v1/products", async (AppDbContext context, ListingRequest listi
     await context.SaveChangesAsync();
 
     return Results.Accepted($"api/v1/productstatus/{listingRequest.RequestId}", listingRequest);
+});
+
+
+//Status endpoint
+app.MapGet("api/v1/productstatus/{requestId}", (AppDbContext context, string requestId) =>
+{
+    var listingRequest = context.ListingRequests.FirstOrDefault(lr => lr.RequestId == requestId);
+
+    if (listingRequest is null) return Results.NotFound();
+
+    ListingStatus listingStatus = new ListingStatus
+    {
+        RequestStatus = listingRequest.RequestStatus,
+        ResourceURL = String.Empty
+    };
+
+    if (listingRequest.RequestStatus!.ToUpper() == "COMPLETE")
+    {
+        listingStatus.ResourceURL = $"api/v1/products/{Guid.NewGuid().ToString()}";
+        return Results.Redirect("https://localhost:7273/" + listingStatus.ResourceURL);
+    }
+
+    listingStatus.EstimatedCompletionTime = "2023-02-06:15:00:00";
+    return Results.Ok(listingStatus);
+
+});
+
+
+//Final EndPoint
+app.MapGet("api/v1/products/{requestId}", (string requestId) =>
+{
+    return Results.Ok("This is where you would pass back the final result");
 });
 
 app.Run();
